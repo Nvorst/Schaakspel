fa7704f949d88e2428da7990367dfd7e52b93387
Niko van der Vorst <niko.vander.vorst@capgemini.com>
2019-02-14 12:46:45 +0100

    Initial commit

---
+++ src/main/java/com/cap/Schaakspel/SchaakspelApplication.java
@@ -0,0 +1,14 @@
+package com.cap.Schaakspel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchaakspelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchaakspelApplication.class, args);
+	}
+
+}
+
